f0349d7511eb53394d3d05f0a67366fa9fb0efa5
Chiara <chiaraiors@gmail.com>
2022-01-04 16:03:08 +0100

    add explore topics

--- resources/views/series/explore_topics.blade.php
+++ resources/views/series/explore_topics.blade.php
@@ -5,4 +5,23 @@
 </div>
 <!-- /.section_title -->
 
+<div class="explore_topics_grid d_g">
+    @foreach($item['info'] as $index=>$serie)
+    <div class="topic_card d_f align_items_center">
+        <div class="d_f align_items_center">
+            <img src="{{$serie['thumb']}}" alt="{{$serie['name']}}">
+            <div>
+                <h5>{{$serie['name']}}</h5>
+                <div class="d_f align_items_center">
+                    <span>{{$serie['series']}} Series</span>
+                    <span class="dot"> • </span>
+                    <span>{{$serie['videos']}} Video</span>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- /.topic_card -->
+    @endforeach
+</div>
+<!-- /.explore_topics_grid -->
 @endforeach
